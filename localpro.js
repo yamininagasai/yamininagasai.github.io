f90a023080f8f5122570db2ab790698db78e0559
yamininagasai <54571401+yamininagasai@users.noreply.github.com>
2019-09-06 11:15:06 +0530

    my first commit

---
+++ localpro.js
@@ -0,0 +1,64 @@
+function totalCalc(){			//calculating the product price depending on quantity
+	var q = document.getElementById("quant").value;
+	var price=document.getElementById("price").value;
+	if (q>5){
+		alert ("you can only add 5 quantities");
+		document.getElementById("quant").value="";
+		document.getElementById("total").value="";
+		}
+	else{
+		var rupees=(q*price);
+		cost=rupees.toFixed(2);
+		document.getElementById("total").value = cost;
+		document.getElementById("total").disabled=true;
+		}
+	}
+function cook(){	//saving the product details in the localStorage
+	var rupees = document.getElementById("total").value;
+	var tit= document.title;					//tit=doll
+	var id=document.getElementById("demo").value;
+	var quant=document.getElementById("quant").value;
+	if(quant.length==0){
+		alert("Please enter quantity of products");
+		return false;
+		}
+	else{
+		var myPro={title: tit, quantity:quant, id: id, price:rupees};
+		var myJSON= JSON.stringify(myPro);			
+		var productcount=localStorage.length;
+		if(productcount==0){
+			localStorage.setItem("product1",myJSON);
+			}
+		else if(productcount<=16){
+			var x = Math.floor((Math.random() * 10000) + 1);
+            var i=0, j=0;
+            while(i<productcount){
+			var key=localStorage.key(i);
+			if(key=="price"){
+				i++;
+				j++;
+				if(j==productcount){
+					localStorage.setItem("product"+x, myJSON);
+					}
+				}
+			else{
+				var product=localStorage.getItem(key);
+				var data=JSON.parse(product);
+				var producttitle=data.id;
+				if(producttitle!=id){
+					i++;
+					j++;
+					if(j==productcount){
+						localStorage.setItem("product"+x, myJSON);
+						}
+					}
+				else if(producttitle==id){
+					localStorage.setItem(key, myJSON);
+					return true;
+					}
+				}
+			}
+		}
+		return true;
+		}
+	}
